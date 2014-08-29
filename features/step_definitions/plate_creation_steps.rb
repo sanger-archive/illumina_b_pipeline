0b5f4551d35592c2ac5c7119b50b79bbe9970dfc
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2014-08-29 16:24:56 +0100

    Fix references to text inside views from cucumber tests.

--- features/step_definitions/plate_creation_steps.rb
+++ features/step_definitions/plate_creation_steps.rb
@@ -1,9 +1,9 @@
 When /^I enter a valid user barcode$/ do
-  fill_in("User Barcode", :with => "12343456454543")
+  fill_in("User Swipecard", :with => "12343456454543")
 end
 
 When /^I enter a valid Source Plate barcode$/ do
-  fill_in("Source Plate Barcode", :with => "22343456454543")
+  fill_in("Plate or Tube Barcode", :with => "22343456454543")
 end
 
 Then /^I am presented with a screen allowing me to create a destination plate$/ do
